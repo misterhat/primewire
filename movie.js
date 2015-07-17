5b8d51f4cc983bdd7e27ddd2f0dec881d067f0f9
Evan Goode <mail@evangoo.de>
2015-07-17 01:45:13 -0400

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,12 @@
+var primewire = require('primewire');
+var sys = require('sys')
+var exec = require('child_process').exec;
+var args = process.argv.slice(2);
+
+function display(err, links, id) {
+	exec('open ' + links[0]);
+}
+
+primewire({
+	title: args[0],
+}, display);
